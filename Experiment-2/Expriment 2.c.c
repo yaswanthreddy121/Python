6e29f0b36633ecf6c04445c838fc5e5f36a2a614
yaswanthreddy121 <147810981+yaswanthreddy121@users.noreply.github.com>
2025-03-28 15:27:47 +0530

    Add files via upload

---
+++ Experiment-2/Expriment 2.c.c
@@ -0,0 +1,52 @@
+num=int(input("Enter a number:"))
+
+if num==0:
+
+    print("zero")
+
+elif num==1:
+
+    print("one")
+
+elif num==2:
+
+    print("two")
+
+elif num==3:
+
+    print("three")
+
+elif num==4:
+
+    print("four")
+
+elif num==5:
+
+    print("five")
+
+elif num==6:
+
+    print("six")
+
+elif num==7:
+
+    print("seven")
+
+elif num==8:
+
+    print("eight")
+
+elif num==9:
+
+    print("nine")
+
+else:
+
+    print("Number out of range:")
+ 
+
+Output:
+Enter a number:3
+
+three
+   
